1b605a9c401e2dcd1230f182684eae2610f2f9bf
Tetiana Parashchenko <tetiana.parashchenko@boldcommerce.com>
2024-07-15 18:35:11 -0500

    Checkout order test

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cypress-tests
-E2E automated tests created using Cypress
+E2E automated tests created using Cypress.
+
+Tests are created totally random without requirements.
+Tests list:
+* DemoBlaze page home opened
+* Select product in store
+* 
--- cypress/e2e/shop.cy.js
+++ cypress/e2e/shop.cy.js
@@ -1,6 +1,6 @@
 import products from '../fixtures/products.json'
 
-describe('DemoBlaze page home', () => {
+describe('DemoBlaze page home opened', () => {
   it('DemoBlaze home page is loaded', () => {
     cy.visit('')
     cy.url().should('include', 'demoblaze')
@@ -14,5 +14,19 @@ describe('DemoBlaze page home', () => {
     })
     cy.get('h2').should('have.text', products.product1.name)
     cy.contains('.price-container', products.product1.price)
+  })
+
+  it('Complete order with selected product', () => {
+    cy.visit('/prod.html', {
+      qs: {
+        idp_: products.product1.product_id
+      },
+    })
+    cy.get('.btn-success').click()
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains('Product added');
+    })
+    cy.visit('cart.html')
+    cy.contains()
   })
 })
